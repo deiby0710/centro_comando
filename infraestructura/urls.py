41440b2d21d9e00a11c2d6d46984a088f95e8924
deiby0710 <deibyalejandro10@hotmail.com>
2026-09-04 18:33:31 -0500

    CRUD completo

--- infraestructura/urls.py
+++ infraestructura/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import lista_servidores, detalle_servidor
+from .views import crear_servidor, editar_servidor, eliminar_servidor, lista_servidores, detalle_servidor
 
 urlpatterns = [
     path('', lista_servidores, name='home_servidores'),
     path('servidor/<int:pk>', detalle_servidor, name='detalle_servidor'),
+    path('servidor/nuevo/', crear_servidor, name='crear_servidor'),
+    path('servidor/<int:pk>/editar/', editar_servidor, name='editar_servidor'),
+    path('servidor/<int:pk>/eliminar/', eliminar_servidor, name='eliminar_servidor'),
 ]
--- infraestructura/views.py
+++ infraestructura/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
+from infraestructura.forms import NodoServidorForm
 from .models import NodoServidor
+
+def eliminar_servidor(request, pk):
+    nodo = get_object_or_404(NodoServidor, pk=pk)
+    if request.method == 'POST':
+        nodo.delete()
+        return redirect('home_servidores')
+    return render(request, 'infraestructura/eliminar_servidor.html', {'nodo': nodo})
+
+def editar_servidor(request, pk):
+    nodo = get_object_or_404(NodoServidor, pk=pk)
+    if request.method == 'POST':
+        form = NodoServidorForm(request.POST, instance=nodo)
+        if form.is_valid():
+            form.save()
+            return redirect('detalle_servidor', pk=nodo.pk)
+    else:
+        form = NodoServidorForm(instance=nodo)
+    return render(request, 'infraestructura/editar_servidor.html', {'form': form, 'nodo': nodo})
+
+def crear_servidor(request):
+    if request.method == 'POST':
+        form = NodoServidorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home_servidores')
+    else:
+        form = NodoServidorForm()
+    return render(request, 'infraestructura/crear_servidor.html', {'form': form})
+
 
 def detalle_servidor(request, pk):
 	nodo = get_object_or_404(NodoServidor, pk=pk)
